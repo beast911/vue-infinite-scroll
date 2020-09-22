c21b4ebcbe5a8f1f7a583f36b196263262718989
KANWAR UDAYBIR SINGH <kanwarudaybir.singh@trip.com>
2020-09-22 17:10:03 +0800

    update expr

--- src/directive.js
+++ src/directive.js
@@ -116,12 +116,13 @@ var doBind = function() {
   var disabled = false;
 
   if (disabledExpr) {
-    this.vm.$watch(disabledExpr, function(value) {
+    const unwatch = this.vm.$watch(disabledExpr, function(value) {
       directive.disabled = value;
       if (!value && directive.immediateCheck) {
         doCheck.call(directive);
       }
     });
+    directive.unwatch = unwatch;
     disabled = Boolean(directive.vm[disabledExpr]);
   }
   directive.disabled = disabled;
@@ -183,25 +184,27 @@ export default {
   bind(el, binding, vnode) {
     el[ctx] = { el, vm : vnode.context, expression : binding.value };
     const args = arguments;
-    el[ctx].vm.$nextTick(function() {
-      if (isAttached(el)) {
-        doBind.call(el[ctx], args);
-      }
-
-      el[ctx].bindTryCount = 0;
-
-      var tryBind = function() {
-        if (el[ctx].bindTryCount > 10)
-          return; // eslint-disable-line
-        el[ctx].bindTryCount++;
+    el[ctx].vm.$on('hook:mounted', function() {
+      el[ctx].vm.$nextTick(function() {
         if (isAttached(el)) {
           doBind.call(el[ctx], args);
-        } else {
-          setTimeout(tryBind, 50);
         }
-      };
 
-      tryBind();
+        el[ctx].bindTryCount = 0;
+
+        var tryBind = function() {
+          if (el[ctx].bindTryCount > 10)
+            return; // eslint-disable-line
+          el[ctx].bindTryCount++;
+          if (isAttached(el)) {
+            doBind.call(el[ctx], args);
+          } else {
+            setTimeout(tryBind, 50);
+          }
+        };
+
+        tryBind();
+      });
     });
   },
 
